116038f9c3fa49e11578200899ba7adb86e661df
asacco <andres.sacco@travelx.io>
2024-10-07 10:05:32 -0300

    final

--- api-edteam-processor/src/main/java/com/edteam/api/processor/service/ChatService.java
+++ api-edteam-processor/src/main/java/com/edteam/api/processor/service/ChatService.java
@@ -1,8 +1,5 @@
 package com.edteam.api.processor.service;
 
-import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_CONVERSATION_ID_KEY;
-import static org.springframework.ai.chat.client.advisor.AbstractChatMemoryAdvisor.CHAT_MEMORY_RETRIEVE_SIZE_KEY;
-
 import com.edteam.api.processor.dto.*;
 import com.edteam.api.processor.enums.APIError;
 import com.edteam.api.processor.enums.Model;
@@ -10,20 +7,13 @@ import com.edteam.api.processor.exception.EdteamException;
 import com.edteam.api.processor.repository.ProcessorHistoryRepository;
 import com.edteam.api.processor.util.ProcessorUtil;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
-import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.model.ChatModel;
-import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.converter.BeanOutputConverter;
-import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
@@ -64,34 +54,7 @@ public class ChatService {
             if (request.getModel() == Model.LLAMA) {
                 throw new EdteamException(APIError.VALIDATION_ERROR);
             }
-
-            BeanOutputConverter<AnalysisResponseDTO> beanOutputConverter =
-                    new BeanOutputConverter<>(AnalysisResponseDTO.class);
-
-            String format = beanOutputConverter.getFormat();
-
-
-            UserMessage userMessage = new UserMessage(request.getPrompt().concat(" with the format: ").concat(format));
-
-            String response =
-                    getChatClient(request.getModel())
-                            .prompt(
-                                    new Prompt(
-                                            List.of(userMessage),
-                                            OpenAiChatOptions.builder()
-                                                    .withFunction("SalesByPeriod")
-                                                    .build()))
-                            .call()
-                            .content();
-
-            // Update the interaction history
-            if (Objects.nonNull(request.getConversationId())) {
-                repository.save(
-                        new ProcessorHistoryDTO(
-                                request.getConversationId(), request.getPrompt(), response));
-            }
-            return beanOutputConverter.convert(response);
-
+            return new AnalysisResponseDTO();
         } catch (Exception e) {
             throw new EdteamException(APIError.BAD_FORMAT);
         }
@@ -120,60 +83,9 @@ public class ChatService {
     }
 
     private String askToAI(ProcessorDTO request, String fileContent) {
-
-        Prompt prompt = getPrompt(request, fileContent);
-        LOGGER.info(prompt.getInstructions().toString());
-
-        String response =
-                getChatClient(request.getModel())
-                        .prompt()
-                        .user(prompt.toString())
-                        .advisors(
-                                a ->
-                                        a.param(
-                                                        CHAT_MEMORY_CONVERSATION_ID_KEY,
-                                                        request.getConversationId())
-                                                .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY, 100))
-                        .call()
-                        .content();
-
-        // Update the interaction history
-        if (Objects.nonNull(request.getConversationId())) {
-            repository.save(
-                    new ProcessorHistoryDTO(
-                            request.getConversationId(), request.getPrompt(), response));
-        }
+        String response = "";
 
         return response;
     }
 
-    private Prompt getPrompt(ProcessorDTO request, String fileContent) {
-        PromptTemplate promptTemplate = getPromptTemplate();
-
-        Map<String, Object> params =
-                Map.of("fileContent", fileContent, "prompt", request.getPrompt());
-        return promptTemplate.create(params);
-    }
-
-    private PromptTemplate getPromptTemplate() {
-        var template =
-                """
-                I am an analyst of a big company which have different reports.
-                I am mostly interested to analyze the files.
-                Here is the file content I am analyzing:
-                {fileContent}
-                I want only the information {prompt} that I request it.""";
-
-        return new PromptTemplate(template);
-    }
-
-    private ChatClient getChatClient(Model model) {
-        if (model == Model.LLAMA) {
-            return ollamaChatClient;
-
-        } else if (model == Model.OPENAI) {
-            return openaiChatClient;
-        }
-        return ollamaChatClient;
-    }
 }
