6746094f4d189bb1b15afe5f3665056368f2dd22
asacco <andres.sacco@travelx.io>
2024-10-11 08:53:27 -0300

    add changes

--- api-edteam-processor/src/main/java/com/edteam/api/processor/controller/ProcessorHistoryController.java
+++ api-edteam-processor/src/main/java/com/edteam/api/processor/controller/ProcessorHistoryController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @Validated
 @RestController
 public class ProcessorHistoryController implements ProcessorHistoryResource {
